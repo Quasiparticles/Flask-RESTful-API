d3e275bc3accd29515238773d63400262e8e8ab7
kwu4wu@gmail.com <kwu4wu@gmail.com>
2016-12-19 11:45:45 +0800

    Add Authentication and logging in using JWT

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'kevin' #production-api would be secure lol
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
     
     def post(self, name):
-        if item = next(filter(lambda x: x['name'] == name, items), None) is not none:
+        if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
-        
+                
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
