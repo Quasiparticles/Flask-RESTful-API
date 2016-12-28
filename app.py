c5e340c9fd5bb088177eac30a10b527e48ad517e
kwu4wu@gmail.com <kwu4wu@gmail.com>
2016-12-27 17:27:53 -0800

    Improve project structure and maintainability

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from user import UserRegister
-from item import Item, ItemList
+from resources.user import UserRegister
+from resources.item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'kevin' #production-api would be secure lol
@@ -12,7 +12,7 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-api.add_resource(Item, '/item/<string:name>') 
+api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
--- resources/user.py
+++ resources/user.py
@@ -6,41 +6,41 @@ class User:
         self.id = _id
         self.username = username
         self.password = password
-    
+
     @classmethod
     def find_by_username(cls, username):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
-        
+
         query = "SELECT * FROM users WHERE username=?"
         result = cursor.execute(query, (username,))
         row = result.fetchone()
         if row:
             user = cls(*row)
-        else: 
+        else:
             user = None
-        
+
         connection.close()
         return user
-    
+
     @classmethod
     def find_by_id(cls, _id):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
-        
+
         query = "SELECT * FROM users WHERE id=?"
         result = cursor.execute(query, (_id,))
         row = result.fetchone()
         if row:
             user = cls(*row)
-        else: 
+        else:
             user = None
-        
+
         connection.close()
         return user
 
 class UserRegister(Resource):
-    
+
     parser = reqparse.RequestParser()
     parser.add_argument('username',
         type=str,
@@ -52,21 +52,20 @@ class UserRegister(Resource):
         required=True,
         help="This field cannot be blank."
     )
-    
+
     def post(self):
         data = UserRegister.parser.parse_args()
-        
+
         if User.find_by_username(data['username']) is not None:
             return {"message": "A user with that username already exists"}, 400
-        
+
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
-        
+
         query = "INSERT INTO users VALUES (NULL, ?, ?)"
         cursor.execute(query, (data['username'], data['password']))
-        
+
         connection.commit()
         connection.close()
-        
+
         return {"message": "User created successfully."}, 201
-    
