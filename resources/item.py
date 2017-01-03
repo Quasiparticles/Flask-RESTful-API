b8ef801e4bcf596af27ac7eecc8e289de7fd590d
kwu4wu@gmail.com <kwu4wu@gmail.com>
2017-01-02 19:04:11 -0800

    Add StoreModel and Store Resource

--- resources/item.py
+++ resources/item.py
@@ -10,6 +10,11 @@ class Item(Resource):
         required=True,
         help="This field cannot be left blank!"
     )
+    parser.add_argument('store_id',
+        type=float,
+        required=True,
+        help="Every item needs a store id."
+    )
 
     @jwt_required()
     def get(self, name):
@@ -24,7 +29,7 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, data['price'], data['store_id'])
 
         try:
             item.save_to_db()
@@ -48,7 +53,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
 
