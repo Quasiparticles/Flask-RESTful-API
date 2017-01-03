993b7dfde3fd512030047f5bf83a5f1cf31735cd
kwu4wu@gmail.com <kwu4wu@gmail.com>
2017-01-02 20:04:55 -0800

    Update gitignore and remove cache

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
-
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
